0ea5259d1e22688b8970b1db97ee6261ddcccd65
Tien <tmtien0103@gmail.com>
2023-06-02 16:33:35 +0700

    add Sidebar index.jsx team, contacts, invoices, profile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import Topbar from './scenes/global/Topbar';
+import Sidebar from './scenes/global/Sidebar';
+import Dashboard from './scenes/dashboard';
+import Team from './scenes/team';
+import Contact from './scenes/contacts';
+import Invoices from './scenes/invoices';
+import Form from './scenes/form';
+// import Bar from './scenes/bar';
+// import Line from './scenes/line';
+// import Pie from './scenes/pie';
+// import FAQ from './scenes/faq';
+// import Geography from './scenes/geography';
+// import Calendar from './scenes/calendar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [theme, colorMode] = useMode();
+    const [isSidebar, setIsSidebar] = useState(true);
+
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <div className="app">
+                    <Sidebar isSidebar={isSidebar} />
+                    <main className="content">
+                        <Topbar setIsSidebar={setIsSidebar} />
+                        <Routes>
+                            <Route path="/" element={<Dashboard />} />
+                            <Route path="/team" element={<Team />} />
+                            <Route path="/contacts" element={<Contact />} />
+                            <Route path="/invoices" element={<Invoices />} />
+                            <Route path="/form" element={<Form />} />
+                            {/* <Route path="/bar" element={<Bar />} /> */}
+                            {/* <Route path="/pie" element={<Pie />} /> */}
+                            {/* <Route path="/line" element={<Line />} /> */}
+                            {/* <Route path="/faq" element={<FAQ />} /> */}
+                            {/* <Route path="/geography" element={<Geography />} /> */}
+                            {/* <Route path="/calendar" element={<Calendar />} /> */}
+                        </Routes>
+                    </main>
+                </div>
+            </ThemeProvider>
+        </ColorModeContext.Provider>
+    );
 }
 
 export default App;
---
+++ src/scenes/dashboard/index.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material';
+
+const Dashboard = () => {
+    return (
+        <Box m="20px">
+            <Box display="flex" justifyContent="space-between" alignItems="center">
+                <header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+            </Box>
+        </Box>
+    );
+};
+
+export default Dashboard;
